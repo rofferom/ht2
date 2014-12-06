39cc1bac847d2c0403b1c2541d86b0e2992781de
Romain Roffé <rofferom@gmail.com>
2014-12-06 20:24:24 +0100

    [DEV] Add gtest 1.7.0

---
+++ external/gtest-1.7.0/local.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := gtest
+
+LOCAL_C_INCLUDES := \
+	-I $(LOCAL_PATH)/include
+
+LOCAL_EXPORT_C_INCLUDES := \
+	$(LOCAL_C_INCLUDES)
+
+LOCAL_SRC_FILES := \
+	src/gtest.cc \
+	src/gtest-death-test.cc \
+	src/gtest-filepath.cc \
+	src/gtest-port.cc \
+	src/gtest-printers.cc \
+	src/gtest-test-part.cc \
+	src/gtest-typed-test.cc \
+	src/gtest_main.cc
+
+LOCAL_CFLAGS := \
+	-DGTEST_HAS_PTHREAD=1 \
+	-DGTEST_OS_LINUX=1
+
+ifeq ("$(TARGET_OS)","LINUX")
+	LOCAL_EXPORT_LDLIBS := -lpthread
+endif
+
+include $(BUILD_STATIC_LIBRARY)
