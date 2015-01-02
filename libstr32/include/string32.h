db452917a1f4e64cb0fc8befa912d389dd779a42
Romain Roffé <rofferom@gmail.com>
2015-01-02 10:45:22 +0100

    [DEV] Fix string32 C++ protection defines

--- libstr32/include/string32.h
+++ libstr32/include/string32.h
@@ -3,18 +3,20 @@
 
 #include <stdlib.h>
 
-#ifdef __cpluscplus
+#ifdef __cplusplus
 extern "C" {
 #endif // __cpluscplus
 
-#ifndef char32_t
-typedef __CHAR32_TYPE__ char32_t;
-#endif
+#ifndef __cplusplus
+	#ifndef char32_t
+		typedef __CHAR32_TYPE__ char32_t;
+	#endif // !char32_t
+#endif // !__cplusplus
 
 char32_t *memchr32(char32_t *s, char32_t c, size_t n);
 
-#ifdef __cpluscplus
-} extern "C"
+#ifdef __cplusplus
+} // extern "C"
 #endif // __cpluscplus
 
 #endif // !__STRING32_H__
